9872dbab61bd7b3431fb4fae563208cb260f691a
Monisha Pushpanathan <monishap@mit.edu>
2019-11-02 12:59:10 -0400

    Speech to text update

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # AudioQuery
 
-# SpeechToTextModule
+## Installation Instructions
+### SpeechToTextModule
 The speech to text module require installing the following python packages:
-1. SpeechRecognition
+1. SpeechRecognition (https://pypi.org/project/SpeechRecognition/?fbclid=IwAR1AAtRofCY66RIU5uBqf8Pyegi6bUhDIs9kaJveM6jPgRMo7gb7cvIk63k)
 2. Visual Studio CPP Build Tools v14.0 (https://visualstudio.microsoft.com/visual-cpp-build-tools/)
-3. PyAudio (depends on 2)
+3. PyAudio: This installation dependa on (2) above. To install PyAudio on Windows:
+   Go to https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio and download the appropriate PyAudio version for 
+   your Python version and OS version (32bit vs 64 bit)
+   Then follow the steps here: https://www.youtube.com/watch?v=CeFWBzzcj8o
+   We are going with the above hack because pip install PyAudio doesnt work. We also tried installing PyAudio using anaconda 
+   and using that, but that dint work either.
 
--- SpeechToTextModule.py
+++ SpeechToTextModule.py
@@ -1,18 +1,23 @@
 import speech_recognition as sr 
-class SpeechToTextModule() {
-
-    def __init__( ) {
-
-    }
-        
-    def convertSpeechToText() {
+class SpeechToTextModule():
+    
+    def convertSpeechToText( self ):
         r = sr.Recognizer()
         with sr.Microphone() as source:
-            print("SAY SOMETHING: ");
-            audio = r.listen(source);
-            print("TIME OVER THANKS");
+            print("SAY SOMETHING: ")
+            audio = r.listen(source)
+            print("TIME OVER THANKS")
         try:
-            print("TEXT: "+r.recognize_google(audio))
-    }
+            text = r.recognize_google(audio)
+            print("TEXT: ", text)
+            return text
+        except Exception as ex:
+            print (ex)
 
-}
+if __name__ == "__main__":
+    # start = time.time()
+    stt  = SpeechToTextModule()
+    text = stt.convertSpeechToText()
+    # end = time.time()
+    # print("Time Taken: ", start-end)
+    print("End")

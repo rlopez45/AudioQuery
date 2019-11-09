727deb63f54f27a865a320ed77c6d684553d64f2
Monisha Pushpanathan <monishap@mit.edu>
2019-11-09 13:37:10 -0500

    minor changes

--- SpeechToTextModule.py
+++ SpeechToTextModule.py
@@ -1,23 +1,33 @@
 import speech_recognition as sr 
+import time
+
 class SpeechToTextModule():
-    
+    '''
+    Converts an audio input into text using the Google Speech to Text API
+    input:
+    Audio as spoken into the microphone
+    returns:
+    The text (converted from audio)
+    '''   
     def convertSpeechToText( self ):
         r = sr.Recognizer()
         with sr.Microphone() as source:
             print("SAY SOMETHING: ")
+            # listen for the first phrase and extract it into audio data
             audio = r.listen(source)
             print("TIME OVER THANKS")
         try:
             text = r.recognize_google(audio)
             print("TEXT: ", text)
             return text
-        except Exception as ex:
-            print (ex)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 if __name__ == "__main__":
-    # start = time.time()
-    stt  = SpeechToTextModule()
-    text = stt.convertSpeechToText()
-    # end = time.time()
-    # print("Time Taken: ", start-end)
-    print("End")
+    start = time.time()
+    stt   = SpeechToTextModule()
+    text  = stt.convertSpeechToText()
+    end   = time.time() 
+    print("Time Taken(s): ", end-start)
--- similarityHelper.py
+++ similarityHelper.py
@@ -1,4 +1,5 @@
-
+#  Find the token associated with a for each token occurence
+#  We assume the token associated with for each will occur right after 
 def findForEach(list_tokens):
     for i in range(len(list_tokens)-1):
         curr = list_tokens[i][0]
@@ -7,6 +8,9 @@ def findForEach(list_tokens):
             if i+2<len(list_tokens):
                 return list_tokens[i-2]
     return None
+
+#  Extract noun token from a list of tokens
+#  Token is a tuple of the form ( token string, part of speech )
 def getNouns(list_tokens):
     nouns = []
     for token in list_tokens:
@@ -14,11 +18,15 @@ def getNouns(list_tokens):
         if 'n' in pos.lower():
             nouns.append(token)
     return nouns
+
+#  Strip lines from a file of leading and trailing whitespaces
 def parseSQL(filename):
     with open(filename) as f:
         lines = f.readlines()
     lines = [line.strip() for line in lines]
     return lines
+
+
 if __name__ == "__main__":
     filename = "SupportedSQLCommands.txt"
     result = parseSQL(filename)
